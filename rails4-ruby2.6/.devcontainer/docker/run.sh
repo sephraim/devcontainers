a6fb2add9bc9437ba40b1edf79a3b713a1dd437a
Sean Ephraim <808563+sephraim@users.noreply.github.com>
2024-02-21 19:08:14 -0600

    Fix rdbg rails startup command

--- rails4-ruby2.6/.devcontainer/docker/run.sh
+++ rails4-ruby2.6/.devcontainer/docker/run.sh
@@ -6,4 +6,4 @@ set -eu
 # bundle exec rdbg --open -n -c -- bundle exec rails server -p 3000 -b 0.0.0.0
 
 # If 'debug' is not in your Gemfile:
-rdbg --open -n -c -- bundle exec rails server -p 3000 -b 0.0.0.0
+rdbg --open -n -c -- bin/rails server -p 3000 -b 0.0.0.0
