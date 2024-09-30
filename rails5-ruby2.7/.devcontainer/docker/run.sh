cb7b127b305a4784ab59b040947002475fc9fd1f
Sean Ephraim <808563+sephraim@users.noreply.github.com>
2024-09-30 12:05:41 -0500

    Upgrade to Ruby 2.7.7 + lots of minor edits

--- rails5-ruby2.7/.devcontainer/docker/run.sh
+++ rails5-ruby2.7/.devcontainer/docker/run.sh
@@ -2,8 +2,10 @@
 
 set -eu
 
-# If 'debug' is in your Gemfile:
-# bundle exec rdbg --open -n -c -- bundle exec rails server -p 3000 -b 0.0.0.0
-
-# If 'debug' is not in your Gemfile:
-rdbg --open -n -c -- bin/rails server -p 3000 -b 0.0.0.0
+if bundle show rdbg > /dev/null 2>&1; then
+  # If 'rdbg' is in your Gemfile:
+  bundle exec rdbg --open -n -c -- bin/rails server -p 3000 -b 0.0.0.0
+else
+  # If 'rdbg' is not in your Gemfile:
+  rdbg --open -n -c -- bin/rails server -p 3000 -b 0.0.0.0
+fi
